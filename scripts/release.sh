4114d4d897d8d62554544cf5617a1c0aef4ab352
mikeborodin <mikeborodin.dev@gmail.com>
2023-02-26 18:07:35 +0100

    [update] terrun.rb

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 scripts/build.sh
+
 cd build
 tar -czf terrun.tar.gz terrun
 sha=`shasum -a 256 terrun.tar.gz | cut -d " " -f 1`
@@ -12,18 +13,19 @@ cp -r README.md ../homebrew-terrun/README.md
 
 echo "
 class Terrun < Formula
-  desc "terrun - efficient terminal runner"
-  homepage "https://github.com/mikeborodin/terrun"
-  url "https://github.com/mikeborodin/terrun/releases/download/$version/terrun.tar.gz"
-  sha256 "$sha"
-  license "MIT"
-  version "$version"
+  desc \"terrun - efficient terminal runner\"
+  homepage \"https://github.com/mikeborodin/terrun\"
+  url \"https://github.com/mikeborodin/terrun/releases/download/$version/terrun.tar.gz\"
+  sha256 \"$sha\"
+  license \"MIT\"
+  version \"$version"
 
   def install
-    bin.install "terrun"
+    bin.install \"terrun\"
   end
 end""" > ../homebrew-terrun/Formula/terrun.rb
 
+cd ../homebrew-terrun
 git add .
 git commit -m "release $version"
 git push origin main
